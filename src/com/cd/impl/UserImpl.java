bd5f428bec8600cefd01a97cb8e4ae251f2dd85a
zuo <362993181@qq.com>
2015-10-28 09:32:23 +0800

    test

--- src/com/cd/impl/UserImpl.java
+++ src/com/cd/impl/UserImpl.java
@@ -4,7 +4,6 @@ import com.cd.in.ItUser;
 
 public class UserImpl implements ItUser {
 
-	@Override
 	public String getUser() {
 		// TODO Auto-generated method stub
 		return null;
--- src/com/cd/impl/UserOneImpl.java
+++ src/com/cd/impl/UserOneImpl.java
@@ -4,7 +4,6 @@ import com.cd.in.ItUser;
 
 public class UserOneImpl implements ItUser {
 
-	@Override
 	public String getUser() {
 		// TODO Auto-generated method stub
 		return null;
